5dd0df6f9560c3153bda08ed485c5a69fcc82a45
Aggelis Sarbanis <sarbanis@iphost.gr>
2015-06-03 15:54:27 +0300

    Creating a View Class

--- HomeView.js
+++ HomeView.js
@@ -1,13 +1,5 @@
 var HomeView = function(store) {
-	this.initialize = function() {
-        // Define a div wrapper for the view. The div wrapper is used to attach events.
-        this.el = $('<div/>');
-        this.el.on('keyup', '.search-key', this.findByName);
-    };
- 
-    this.initialize();
-    
-    this.render = function() {
+ 	this.render = function() {
 		this.el.html(HomeView.template());
 		return this;
 	}; 
@@ -17,6 +9,14 @@ var HomeView = function(store) {
 			$('.employee-list').html(HomeView.liTemplate(employees));
 		});
 	};
+	this.initialize = function() {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
+    this.initialize();
+    
+    
 }
  
 HomeView.template = Handlebars.compile($("#home-tpl").html());
