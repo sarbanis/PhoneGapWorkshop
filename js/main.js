f1c7fbbf48d8101acb89ce2c591cd0859cf5c63b
Aggelis Sarbanis <sarbanis@iphost.gr>
2015-06-03 15:41:10 +0300

    Creating a View Class

--- js/main.js
+++ js/main.js
@@ -6,22 +6,10 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-    findByName: function() {
-        var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-    },
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-    	$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},	
-    initialize: function() {
-    	this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-		var self = this;
+    initialize: function() {    
+    	var self = this;
 		this.store = new MemoryStore(function() {
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
     }
 };
